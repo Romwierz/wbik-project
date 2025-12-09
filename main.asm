3e144ae14df11d3c02528b8a073ff66e09da5bc8
Romwierz <miczu47@gmail.com>
2025-12-09 13:48:58 +0100

    print strings in each line of lcd

--- main.asm
+++ main.asm
@@ -1,11 +1,28 @@
+    extrn code(init_LCD,LCD_XY,zapisz_string_LCD)
+
     org 8000h
     jmp start
     org 8500h
 
 start:
-    mov a, # 100
-    add a, # 5
-    nop
+    lcall init_LCD
 
+    mov A, #00000000b
+    lcall LCD_XY
+    mov DPTR, #imie
+    lcall zapisz_string_LCD
+
+    mov A, #01000000b
+    lcall LCD_XY
+    mov DPTR, #nazwisko
+    lcall zapisz_string_LCD
+    jmp $
+
+imie:
+    DB 'bajo#'
+
+nazwisko:
+    DB 'jajo#'
+	
     end
 
